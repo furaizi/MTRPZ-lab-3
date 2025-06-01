7f2b583ac2a647edf2d0aae27e6b1528b363153c
furaizi <furachannel16@gmail.com>
2025-06-01 16:05:21 +0300

    Revert changes in main.py

--- python/spaceship/main.py
+++ python/spaceship/main.py
@@ -2,7 +2,3 @@ from spaceship.app import make_app
 from spaceship.config import Settings
 
 app = make_app(Settings())
-
-@app.on_event("startup")
-async def startup_event():
-    print("Application is starting up...")
---
+++ test.sh
@@ -0,0 +1,2 @@
+REPOSITORY   TAG       IMAGE ID       CREATED          SIZE
+lab3         bad       1d0f9b279b1f   36 seconds ago   1.4GB
