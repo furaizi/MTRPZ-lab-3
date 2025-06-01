14338d78a196c939a4332ff19a18546edc24eda9
furaizi <furachannel16@gmail.com>
2025-06-01 15:55:11 +0300

    Make changes to spaceship/main.py

--- python/spaceship/main.py
+++ python/spaceship/main.py
@@ -2,3 +2,7 @@ from spaceship.app import make_app
 from spaceship.config import Settings
 
 app = make_app(Settings())
+
+@app.on_event("startup")
+async def startup_event():
+    print("Application is starting up...")
